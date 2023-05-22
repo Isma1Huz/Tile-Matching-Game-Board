4ace15d098af2dd87fc0ee0cdecae32ae2043e30
Isma1Huz <ismael.hussein@student.moringaschool.com>
2023-05-22 20:24:34 +0300

    Making the start button work

--- script.js
+++ script.js
@@ -10,3 +10,34 @@ const section = document.querySelector("section");
 let interval;
   
 
+
+//Initial Time
+let seconds = 0,
+minutes = 0;
+//Start game
+startBtn.addEventListener("click", () => {
+  movesCount = 0;
+  seconds = 0;
+  minutes = 0;
+  start.classList.add("hide");
+  stopBtn.classList.remove("hide");
+  startBtn.classList.add("hide");
+  interval = setInterval(timeGenerator, 1000);
+  moves.innerHTML = `${movesCount}`;
+  initial();
+  ratingStars();
+});
+
+//Stop game
+stopBtn.addEventListener(
+  "click",
+  (stopGame = () => {
+    start.classList.remove("hide");
+    stopBtn.classList.add("hide");
+    startBtn.classList.remove("hide");
+    clearInterval(interval) ;
+  })
+);
+
+
+
