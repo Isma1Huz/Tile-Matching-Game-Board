450b209f603995e6ebc01619ebcd15486f0dfb35
Isma1Huz <ismael.hussein@student.moringaschool.com>
2023-05-22 15:53:55 +0300

    Creating the page skeleton

---
+++ script.js
@@ -0,0 +1,236 @@
+const moves = document.getElementById("moves");
+const startBtn = document.getElementById("startBtn");
+const stopBtn = document.getElementById("stopBtn");
+const start = document.querySelector(".start-container");
+const time = document.getElementById("time");
+const card = document.querySelectorAll(".card");
+const rates = document.getElementById("stars");
+const restartBtn = document.getElementById("restartBtn");
+const section = document.querySelector("section");
+let interval;
+  
+
+
+
+//Initial Time
+let seconds = 0,
+minutes = 0;
+//Start game
+startBtn.addEventListener("click", () => {
+  movesCount = 0;
+  seconds = 0;
+  minutes = 0;
+  start.classList.add("hide");
+  stopBtn.classList.remove("hide");
+  startBtn.classList.add("hide");
+  interval = setInterval(timeGenerator, 1000);
+  moves.innerHTML = `${movesCount}`;
+  initial();
+  ratingStars();
+});
+
+//Stop game
+stopBtn.addEventListener(
+  "click",
+  (stopGame = () => {
+    start.classList.remove("hide");
+    stopBtn.classList.add("hide");
+    startBtn.classList.remove("hide");
+    clearInterval(interval) ;
+  })
+);
+
+
+restartBtn.addEventListener("click", () => {
+  restart();
+});
+
+
+
+//For timer
+const timeGenerator = () => {
+seconds += 1;
+//minutes logic
+if (seconds >= 60) {
+  minutes += 1;
+  seconds = 0;
+}
+//format t ime before displaying
+let secondsValue = seconds < 10 ? `0${seconds}` : seconds;
+let minutesValue = minutes < 10 ? `0${minutes}` : minutes;
+time.innerHTML = `${minutesValue}:${secondsValue}`;
+};
+
+
+
+//Initial moves and win count
+let movesCount = 0;
+//For calculating moves
+const movesCounter = () => {
+  movesCount += 1;
+  moves.innerHTML = `${movesCount}`;
+};
+
+
+
+
+// Rating
+let rating;
+maxRate = 5;
+const ratingStars = () => {
+  rating = maxRate;
+  rates.innerHTML = `Rating: ${rating}`;
+  if (movesCount < 7){
+    rating = maxRate;
+    rates.innerHTML = `Rating: ${rating}`;
+  } else if(movesCount => 10 && movesCount <= 13){
+    rating = maxRate - 1;
+    rates.innerHTML = `Rating: ${rating}`;
+  }
+  else if(movesCount > 13 && movesCount <= 20){
+    rating = maxRate - 2;
+    rates.innerHTML = `Rating: ${rating}`;
+  }
+  else {
+    rating = 1;
+    rates.innerHTML = `Rating: ${rating}`;
+  }
+}
+
+
+
+
+// Card Data
+
+const getData = () => [
+    { name: "cat", image: "./images/cat.jpeg" },
+    { name: "chicken", image: "./images/chicken.jpg" },
+    { name: "dove", image: "./images/dove.jpeg" },
+    { name: "cat", image: "./images/cat.jpeg" },
+    { name: "rat", image: "./images/rat.jpg" },
+    { name: "chicken", image: "./images/chicken.jpg" },
+    { name: "duck", image: "./images/duck.jpeg" },
+    { name: "dog", image: "./images/dog.jpg" },
+    { name: "duck", image: "./images/duck.jpeg" },
+    { name: "dove", image: "./images/dove.jpeg" },
+    { name: "dog", image: "./images/dog.jpg" },
+    { name: "rat", image: "./images/rat.jpg" },
+];
+
+
+
+// Randomizing
+const randomize = () => {
+    const cardData =  getData();
+
+    cardData.sort(() => Math.random() - 0.5 );
+    return cardData;
+}
+randomize();
+
+
+// Card Generator Function
+
+const cardGenerator = () => {
+    let cardData = randomize();
+    // HTML
+    cardData.forEach((item) => {
+      const card = document.createElement("div");
+      const face = document.createElement("img");
+      const back = document.createElement("div");
+      card.classList = "card";
+      face.classList = "face";
+      back.classList = "back";
+  
+      // Attach the info to the cards
+      face.src = item.image
+      card.setAttribute("name", item.name);
+      // Attach the cards to the section
+      section.appendChild(card);
+      card.appendChild(face);
+      card.appendChild(back);
+
+      card.addEventListener("click", (e) => {
+          card.classList.toggle("toggle");
+          checkCards(e);
+      })
+    })
+   
+};
+
+
+// Check cards
+
+const checkCards = (e) => {
+    const clicked = e.target;
+    clicked.classList.add('flipped');
+    const flipped = document.querySelectorAll(".flipped");
+    const toggle = document.querySelectorAll(".toggle");
+    console.log(flipped);
+
+    // Tests
+    if (flipped.length === 2){
+        movesCounter();
+        ratingStars();
+        if (flipped[0].getAttribute("name") === flipped[1].getAttribute("name")) {
+                flipped.forEach((card) => {
+                    card.classList.remove("flipped");
+                    card.style.pointerEvents = "none";
+                });
+            }
+            else {
+                flipped.forEach((card) => {
+                    card.classList.remove("flipped");
+                    setTimeout(() => card.classList.remove("toggle"), 1000);
+                    if (movesCount === 20) {
+                      restart("Try Again");
+                  }
+                });
+            }   
+            if (toggle.length === 12 ){
+                restart("You won");
+            }
+    }
+};
+
+const restart = (text) => {
+    let cardData = randomize();
+    movesCount = 0;
+    moves.innerHTML = `${movesCount}`;
+    let faces = document.querySelectorAll(".face");
+    
+    let cards = document.querySelectorAll(".card");
+    section.style.pointerEvents = "none";
+    cardData.forEach((item, index) => {
+        cards[index].classList.remove("toggle");
+
+        // Randomize
+        setTimeout(() => {
+            cards[index].style.pointerEvents = "all";
+            faces[index].src = item.image;
+            cards[index].setAttribute("name", item.name);
+            section.style.pointerEvents = "all";
+
+        }, 1000);
+        clearInterval(interval);
+    });
+    // setTimeout(() => window.alert(text), 100);
+
+
+
+}
+cardGenerator();
+
+
+
+
+
+
+
+
+
+
+
+
+
+
