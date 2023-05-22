d611a477ffffbc592e9329af90c116c66f696003
Isma1Huz <ismael.hussein@student.moringaschool.com>
2023-05-22 20:22:47 +0300

    Adding the elements to the DOM in Javascript

---
+++ script.js
@@ -0,0 +1,12 @@
+const moves = document.getElementById("moves");
+const startBtn = document.getElementById("startBtn");
+const stopBtn = document.getElementById("stopBtn");
+const start = document.querySelector(".start-container");
+const time = document.getElementById("time");
+const card = document.querySelectorAll(".card");
+const rates = document.getElementById("stars");
+const restartBtn = document.getElementById("restartBtn");
+const section = document.querySelector("section");
+let interval;
+  
+
